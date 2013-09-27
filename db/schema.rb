e9a209bbed903b9578be07d7f1828209f55bb023
Lloyd Nimetz <lnimetz@me.com>
2013-09-27 01:01:06 -0700

    added first batch of columns to orgs tables

--- db/schema.rb
+++ db/schema.rb
@@ -11,12 +11,38 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130926204753) do
+ActiveRecord::Schema.define(:version => 20130927070951) do
 
   create_table "orgs", :force => true do |t|
     t.string   "name"
-    t.datetime "created_at", :null => false
-    t.datetime "updated_at", :null => false
+    t.datetime "created_at",           :null => false
+    t.datetime "updated_at",           :null => false
+    t.string   "initials"
+    t.string   "address"
+    t.string   "province"
+    t.string   "city"
+    t.boolean  "int_branch"
+    t.integer  "telephone"
+    t.integer  "fax"
+    t.string   "email"
+    t.string   "website"
+    t.date     "founding_date"
+    t.date     "starting_date"
+    t.string   "legal_type"
+    t.integer  "legal_no"
+    t.string   "cuit"
+    t.boolean  "vat_exemption"
+    t.boolean  "income_exemption"
+    t.boolean  "tax_ded_arg"
+    t.text     "mission"
+    t.text     "objectives"
+    t.string   "chief_operator"
+    t.string   "chief_operator_title"
+    t.string   "board_type"
+    t.integer  "no_emp_full"
+    t.integer  "no_emp_part"
+    t.integer  "no_volunteers_perm"
+    t.boolean  "code_conduct"
   end
 
   add_index "orgs", ["name"], :name => "index_orgs_on_name"

8caa00eeeec3886172134dcc4aedbfa6f3be4ac7
Apprentice <apprentice@dbc30.local>
2013-09-27 20:18:44 -0700

    WIP:trying to show search result in a new page

--- app/controllers/orgs_controller.rb
+++ app/controllers/orgs_controller.rb
@@ -1,19 +1,35 @@
 class OrgsController < ApplicationController
 
  def index
- 	if params[:search].blank?
- 		return @search_results = []
- 	else 	
-	 	@search = Org.search do
-	    fulltext params[:search]
-	  end
-	  @search_results = @search.results
-	  puts @search_results
-	  puts "Should be empty!! #{params[:search]}"
-	end
+ 	  
+  # if params[:keyword].blank?
+  #   return @search_results = []
+  # else  
+  #   search = Org.search do
+  #     fulltext params[:keyword]
+  #   end
+  #   @search_results = search.results  
+  # end
+  # @keyword = params[:keyword] 
+  # redirect_to show_url(params[:keyword]) if params[:keyword]
+  render "orgs/show" if params[:keyword]
  end
 
  def show
- end 
+  # @ur = URI(request.fullpath)
+  # @keyword = /keyword=(.+)&/.match(URI(request.fullpath))
+  if @keyword.blank?
+    return @search_results = []
+  else  
+    search = Org.search do
+      fulltext @keyword
+    end
+    @search_results = search.results  
+  end
+ end
+
+ def search_result
+  
+ end
 
 end

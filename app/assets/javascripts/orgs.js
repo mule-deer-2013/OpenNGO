5c530d9f73e2012b6c74217bc35ea8a6af2e33f3
dbc-apprentice <apprentice@devbootcamp.com>
2013-09-30 16:16:30 -0700

    css and js tweaks

---
+++ app/assets/javascripts/orgs.js
@@ -0,0 +1,92 @@
+
+
+
+
+
+
+$(document).ready(function(){
+
+
+// info tabs and buttons:
+
+  $('#org-homepage').show()
+
+
+  $(".org-header-left").click(function() {
+    $('#org-homepage').show()
+    $('#org-programs').hide()
+    $("#org-financials").hide()
+    $("#org-team").hide()
+    $("#org-affiliations").hide()
+  });
+
+
+
+  $('#org-programs-butt').click(function() {
+    $('#org-homepage').hide()
+    $("#org-programs").show()
+    $("#org-financials").hide()
+    $("#org-team").hide()
+    $("#org-affiliations").hide()
+
+  });
+
+  $("#org-financials-butt").click(function() {
+    $('#org-homepage').hide()
+    $("#org-financials").show()
+    $("#org-programs").hide()
+    $("#org-team").hide()
+    $("#org-affiliations").hide()
+  });
+
+
+  $("#org-team-butt").click(function() {
+    $('#org-homepage').hide()
+    $("#org-financials").hide()
+    $("#org-programs").hide()
+    $("#org-team").show()
+    $("#org-affiliations").hide()
+  });
+
+
+  $("#org-affiliations-butt").click(function() {
+    $('#org-homepage').hide()
+    $("#org-financials").hide()
+    $("#org-programs").hide()
+    $("#org-team").hide()
+    $("#org-affiliations").show()
+  });
+
+
+// text resizer:
+
+$( '.resizer' ).each(function ( i, span ) {
+
+    var width = $( span ).width(),
+        html = '<span style="white-space:nowrap"></span>',
+        line = $( span ).wrapInner( html ).children()[ 0 ],
+        n = 40; //biggest possible font size
+
+    $( span ).css( 'font-size', n );
+
+    while ( $( line ).width() > width ) {
+        $( span ).css( 'font-size', --n );
+    }
+
+    $( span ).text( $( line ).text() );
+
+});
+
+
+
+
+
+
+
+});
+
+
+
+
+
+

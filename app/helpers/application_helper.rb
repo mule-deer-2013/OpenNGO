115993f0651562e4631368ebd2b3ecef9177e2a8
Apprentice <apprentice@dbc20.local>
2013-10-02 09:38:15 -0700

    update HA logo

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,9 +1,9 @@
 module ApplicationHelper
-  def filter(field, value)
-    search_filter = []
-    self.each do |result|
-      search_filter << result if result.field == value
-    end
-    search_filter
-  end
+  # def filter(field, value)
+  #   search_filter = []
+  #   self.each do |result|
+  #     search_filter << result if result.field == value
+  #   end
+  #   search_filter
+  # end
 end

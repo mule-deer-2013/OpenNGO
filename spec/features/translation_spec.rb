c7355106bde9358bf9ea27464f2750df5589928a
Apprentice <apprentice@dbc31.local>
2013-10-14 14:51:26 -0700

    added translations, fixed translation text, and improved facets

--- spec/features/translation_spec.rb
+++ spec/features/translation_spec.rb
@@ -1,11 +1,12 @@
 require 'spec_helper'
 
 feature "translation" do
-  scenario "User switches from english to spanish and back" do
+  scenario "User switches from english to spanish and back in homepage" do
     visit root_path
-    click_link "Castellano"
+    click_link 'Ar'
     expect(current_path).should eql('/es')
-    click_link "English"
+    click_link 'Us'
     expect(current_path).should eql('/en')
   end
+
 end

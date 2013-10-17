3e957c60b6c2860600a589817485617eb7536957
Ale1 <alejandro.barbero@gmail.com>
2013-10-17 16:10:18 -0700

    editing production.rb to make heroku preocmpile charts

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -20,6 +20,8 @@ OpenNGO::Application.configure do
   # Generate digests for assets URLs
   config.assets.digest = true
 
+ config.assets.precompile += %w( highcharts.js )
+
   # Defaults to nil and saved in location specified by config.assets.prefix
   # config.assets.manifest = YOUR_PATH
 
